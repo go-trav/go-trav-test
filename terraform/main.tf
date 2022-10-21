f0efd998156477eacb74dd69fc98241f895fb9a8
mohithworks <mohithworks2002@gmail.com>
2022-10-21 09:31:28 +0530

    Added prod

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region  = "ap-south-1"
+}
+
+terraform {
+  backend "s3" {
+      bucket = "vinayworks"
+      key    = "build/terraform.tfstate"
+      region = "ap-south-1"
+  }
+}
+
+data "aws_iam_policy_document" "website_policy" {
+  statement {
+    actions = [
+      "s3:GetObject"
+    ]
+    principals {
+      identifiers = ["*"]
+      type = "AWS"
+    }
+    resources = [
+      "arn:aws:s3:::gotrav/*"
+    ]
+  }
+}
+
+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
+
+resource "aws_s3_bucket" "s3Bucket" {
+     bucket = "gotrav"
+     acl       = "public-read"
+
+     policy = data.aws_iam_policy_document.website_policy.json
+
+   website {
+       index_document = "index.html"
+   }
+}
